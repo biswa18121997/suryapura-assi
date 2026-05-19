ab0aef75c00f1bae47d1dc8380c24c5e6676830d
biswa18121997 <biswajitshrm6@gmail.com>
2026-05-19 14:44:54 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/Navbar';
 import VillageHeroSection from './components/Componentone.tsx';
@@ -9,7 +6,6 @@ import VillageDevelopmentMetrics from './components/Progress.tsx'
 import VillageSocialFeed from './components/VillageSocialFeed.tsx'
 import VillageFooter from './components/VillageFooter.tsx'
 function App() {
-	const [count, setCount] = useState(0)
 
 	return (
 		<>
